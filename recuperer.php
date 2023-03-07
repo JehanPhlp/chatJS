209e92a5079c37bae4d84d4de89184efeb29a8be
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-07 08:22:58 +0100

    ouais

--- recuperer.php
+++ recuperer.php
@@ -3,14 +3,4 @@
     require('query.php');
 
     $derniersMessages = get10Messages();
-
-    echo '<table>';
-            foreach($derniersMessages as $message){
-                echo '<tr>
-                        <td>'.$message['auteur'].'</td>
-                        <td>'.$message['contenu'].'</td>
-                    </tr>';
-            }
-            echo '</table>';
-
 ?>
