46be4d1ae2ef0dd36390a37f2048432967ce90c5
NoaDespaux <noa.despaux@etu.iut-tlse3.fr>
2023-03-19 17:20:02 +0100

    clean recuperer

--- src/recuperer.php
+++ src/recuperer.php
@@ -1,46 +1,34 @@
+<?php
+    require_once('../data/query.php');
+    $listmessages = get10Messages();
+    echo '<table id="listeMessages">';
+    foreach(array_reverse($listmessages) as $message){
+        echo '
+        <tr>
+            <td>'.$message['auteur'].'</td>
+            <td>'.$message['contenu'].'</td>
+            <td>Il y a '. afficherDate($message['date_heure']).'</td>
+            </tr>';
+    }
+    echo '
+    </table>';
 
-<!doctype html>
-<html lang="fr">
-    <head>
-        <meta charset="utf-8">
-        <script src="script.js"></script>
-        <script src="../dependencies/jquery.js"></script>
-    </head>
-    <body>
-        <?php
-            require_once('../data/query.php');
-            $listmessages = get10Messages();
-            echo '<table id="listeMessages">';
-            foreach(array_reverse($listmessages) as $message){
-                echo '
-                <tr>
-                    <td>'.$message['auteur'].'</td>
-                    <td>'.$message['contenu'].'</td>
-                    <td>Il y a '. afficherDate($message['date_heure']).'</td>
-                    </tr>';
+    function afficherDate($dateHeure){
+        $time = floor((strtotime(date('Y-m-d H:i:s')) - strtotime($dateHeure)) / 60);
+        if ($time > 1440) {
+            if (floor($time/1440) == 1) {
+                return "1 jour";
+            } else {
+                return floor($time/1440)." jours";
             }
-            echo '
-            </table>';
-
-            function afficherDate($dateHeure){
-                $time = floor((strtotime(date('Y-m-d H:i:s')) - strtotime($dateHeure)) / 60);
-                if ($time > 1440) {
-                    if (floor($time/1440) == 1) {
-                        return "1 jour";
-                    } else {
-                        return floor($time/1440)." jours";
-                    }
-                } else if ($time > 60) {
-                    if (floor($time/60) == 1) {
-                        return "1 heure";
-                    } else {
-                        return floor($time / 60)." heures";
-                    }
-                } else {
-                    return $time . " min";
-                }
+        } else if ($time > 60) {
+            if (floor($time/60) == 1) {
+                return "1 heure";
+            } else {
+                return floor($time / 60)." heures";
             }
-        ?>
-    
-    </body>
-</html>
+        } else {
+            return $time . " min";
+        }
+    }
+?>
